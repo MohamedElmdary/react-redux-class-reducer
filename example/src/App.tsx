0e85d98e767e7691b51741198b6706144bf0738a
MohamedElmdary <engm5081@gmail.com>
2020-08-16 14:12:32 +0200

    init commit

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 
-import { ExampleComponent } from 'react-redux-class-reducer'
+import { init } from 'react-redux-class-reducer'
 import 'react-redux-class-reducer/dist/index.css'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  return (
+    <div>
+      <p>{init}</p>
+    </div>
+  )
 }
 
 export default App
